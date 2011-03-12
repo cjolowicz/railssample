d941897090ae0eab5f8feeb999e09ea88b64986c
Claudio Jolowicz <cj@dealloc.org>
2011-03-13 00:40:43 +0100

    Require a name and email for each user.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = { :name => 'Example User', :email => 'user@example.com' }
+  end
+
+  it "should create a new instance given valid attributes" do
+    User.create! @attr
+  end
+
+  it "should require a name" do
+    user = User.new @attr.merge(:name => '')
+    user.should_not be_valid
+  end
+
+  it "should require an email address" do
+    user = User.new @attr.merge(:email => '')
+    user.should_not be_valid
+  end
 end
