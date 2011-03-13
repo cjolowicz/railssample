e6f28772bec442a0546bf407c2d92140a308a1d2
Claudio Jolowicz <cj@dealloc.org>
2011-03-13 13:49:58 +0100

    Add encrypted password attribute.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -85,4 +85,14 @@ describe User do
       user.should_not be_valid
     end
   end
+
+  describe "password encryption" do
+    before(:each) do
+      @user = User.create! @attr
+    end
+
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+  end
 end
