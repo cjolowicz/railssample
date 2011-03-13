3a0ce8c2fc253243c870135714a8cefc2f156a65
Claudio Jolowicz <cj@dealloc.org>
2011-03-13 17:32:42 +0100

    Add user factory. Add test for show action of User controller.

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  'Claudio Jolowicz'
+  user.email                 'claudio@dealloc.org'
+  user.password              'foobar'
+  user.password_confirmation 'foobar'
+end
