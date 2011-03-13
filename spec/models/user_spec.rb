346a9b18c3faff2ac5f4a4390543d34815c66e9d
Claudio Jolowicz <cj@dealloc.org>
2011-03-13 01:08:09 +0100

    Reject names that are too long. Require valid email addresses.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,4 +18,26 @@ describe User do
     user = User.new @attr.merge(:email => '')
     user.should_not be_valid
   end
+
+  it "should reject names that are too long" do
+    name = 'a' * 51
+    user = User.new @attr.merge(:name => name)
+    user.should_not be_valid
+  end
+
+  it "should accept valid email addresses" do
+    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+    addresses.each do |address|
+      user = User.new @attr.merge(:email => address)
+      user.should be_valid
+    end
+  end
+
+  it "should reject invalid email addresses" do
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+    addresses.each do |address|
+      user = User.new @attr.merge(:email => address)
+      user.should_not be_valid
+    end
+  end
 end
