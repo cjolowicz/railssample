a4116b959308bf107246da4305d423edb2c4deef
Claudio Jolowicz <cj@dealloc.org>
2011-03-14 22:29:04 +0100

    Implement sign in failure.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,20 @@ class SessionsController < ApplicationController
   def new
     @title = 'Sign in'
   end
+
+  def create
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      flash.now[:error] = 'Invalid email/password combination.'
+      @title = 'Sign in'
+      render 'new'
+    else
+      # TODO: sign in
+      # TODO: redirect to show
+    end
+  end
+
+  def destroy
+  end
 end
