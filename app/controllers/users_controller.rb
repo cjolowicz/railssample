22456e54701135a3c393917c78313968757be15f
Claudio Jolowicz <cj@dealloc.org>
2011-03-19 15:37:36 +0100

    Prevent admins from deleting themselves.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user,   :only => :destroy
   before_filter :new_user,     :only => [:new, :create]
+  before_filter :other_user,   :only => :destroy
 
   def new
     @user = User.new
@@ -47,7 +48,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
+    @user.destroy
     flash[:success] = 'User destroyed.'
     redirect_to users_path
   end
@@ -60,6 +61,11 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def other_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) if current_user?(@user)
     end
 
     def admin_user
