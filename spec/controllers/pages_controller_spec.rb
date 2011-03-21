524b12669d234ed470221bf0d651380b128a0d91
Claudio Jolowicz <cj@dealloc.org>
2011-03-21 22:44:29 +0100

    Add test for follower/following stats on home page.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,17 +8,31 @@ describe PagesController do
   end
 
   describe "GET 'home'" do
-    before(:each) do
-      get :home
-    end
-
     it "should be successful" do
+      get :home
       response.should be_success
     end
 
     it "should have the right title" do
+      get :home
       response.should have_selector("title",
                                     :content => "#{@base_title} | Home")
+    end
+
+    describe "when signed in" do
+      before(:each) do
+        @user = test_sign_in Factory(:user)
+      end
+
+      it "should have the right follower/following counts" do
+        other_user = Factory(:user, :email => Factory.next(:email))
+        other_user.follow! @user
+        get :home
+        response.should have_selector('a', :href    => following_user_path(@user),
+                                           :content => '0 following')
+        response.should have_selector('a', :href    => followers_user_path(@user),
+                                           :content => '1 follower')
+      end
     end
   end
 
