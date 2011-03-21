6d1ff373bf73bc613ffb564a5467eec252aef89c
Claudio Jolowicz <cj@dealloc.org>
2011-03-21 21:43:49 +0100

    Add test for the User following attribute.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -196,5 +196,9 @@ describe User do
     it "should have a relationships method" do
       @user.should respond_to(:relationships)
     end
+
+    it "should have a following method" do
+      @user.should respond_to(:following)
+    end
   end
 end
