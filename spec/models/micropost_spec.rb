f32f07f17eb9ef456532fd03368dd281f2b57daa
Claudio Jolowicz <cj@dealloc.org>
2011-03-23 19:11:50 +0100

    Add tests for retrieving microposts from users followed by a given user.

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -38,4 +38,33 @@ describe Micropost do
       @user.microposts.build(:content => 'a' * 141).should_not be_valid
     end
   end
+
+  describe "from_users_followed_by" do
+    before(:each) do
+      @followed = Factory(:user, :email => Factory.next(:email))
+      @another  = Factory(:user, :email => Factory.next(:email))
+
+      @user_post     = @user.microposts.create!(:content     => 'Lorem ipsum')
+      @followed_post = @followed.microposts.create!(:content => 'Lorem ipsum')
+      @another_post  = @another.microposts.create!(:content  => 'Lorem ipsum')
+
+      @user.follow! @followed
+    end
+
+    it "should have a from_users_followed_by class method" do
+      Micropost.should respond_to(:from_users_followed_by)
+    end
+
+    it "should include the followed user's microposts" do
+      Micropost.from_users_followed_by(@user).should include(@followed_post)
+    end
+
+    it "should include the user's own microposts" do
+      Micropost.from_users_followed_by(@user).should include(@user_post)
+    end
+
+    it "should not include an unfollowed user's microposts" do
+      Micropost.from_users_followed_by(@user).should_not include(@another_post)
+    end
+  end
 end
